bfd93b2d6ed8525135e1eeaa6772cd82fdbc76dd
gaurav2146 <gaurav2146.gk@gmail.com>
2024-01-28 14:09:02 +0530

    first commit

--- src/Promise_All.ts
+++ src/Promise_All.ts
@@ -1,68 +1,58 @@
 import { CustomPromise } from "./CustomPromise";
 
-export class Promise_All
-{
-    private success_result:string[] = [];
-    private err_result:string;
-    private success_callback:((input?:string[]) => void)[]=[];
-    private error_callback:((input?:string) => void)[]=[];
-    private status:"pending"|"fullfilled"|"rejected"="pending";
+export class Promise_All {
+    private success_result: string[] = [];
+    private err_result: string;
+    private success_callback: ((input?: string[]) => void)[] = [];
+    private error_callback: ((input?: string) => void)[] = [];
+    private status: "pending" | "fullfilled" | "rejected" = "pending";
 
-    all(promises:CustomPromise<string>[]):Promise_All
-    {
-      
-      let number_of_promise = 0;
-      
-      for(let record of promises)
-      {
-        record.then((data:string)=>{
-            this.success_result.push(data);
+    all(promises: CustomPromise<string>[]): Promise_All {
 
-            number_of_promise++;
+        let number_of_promise = 0;
 
-            if(number_of_promise === promises.length)
-            {
-                this.status="fullfilled";
-                this.success_callback.forEach((callback)=>callback(this.success_result));
-            }
+        for (let record of promises) {
+            record.then((data: string) => {
+                this.success_result.push(data);
 
-        }).catch((error:string)=>{
-            this.err_result = error;
-            this.status="rejected";
-            this.error_callback.forEach((callback)=>callback(error));
-        })
-      }
+                number_of_promise++;
 
-      this.all = this.all.bind(this);
+                if (number_of_promise === promises.length) {
+                    this.status = "fullfilled";
+                    this.success_callback.forEach((callback) => callback(this.success_result));
+                }
 
-      return this;
+            }).catch((error: string) => {
+                this.err_result = error;
+                this.status = "rejected";
+                this.error_callback.forEach((callback) => callback(error));
+            })
+        }
+
+        this.all = this.all.bind(this);
+
+        return this;
     }
 
-    then(callback?:(value?:string[])=>void):Promise_All
-    {
-        if(this.status === "rejected")
-        {
-          callback?.(this.success_result);
+    then(callback?: (value?: string[]) => void): Promise_All {
+        if (this.status === "rejected") {
+            callback?.(this.success_result);
         }
-        else if(this.status === "pending")
-        {
-            if(typeof callback === "function")
-            this.success_callback.push(callback);
+        else if (this.status === "pending") {
+            if (typeof callback === "function")
+                this.success_callback.push(callback);
         }
 
         return this;
     }
 
-    catch(callback?:(value?:string)=>void):Promise_All
-    {
-        if(this.status === "rejected")
-        {
-          callback?.(this.err_result);
+    catch(callback?: (value?: string) => void): Promise_All {
+        if (this.status === "rejected") {
+            callback?.(this.err_result);
         }
-        else if(this.status === "pending")
-        {
-            if(typeof callback === "function")
-            this.error_callback.push(callback);
+        else if (this.status === "pending") {
+            if (typeof callback === "function")
+                this.error_callback.push(callback);
         }
 
         return this;
